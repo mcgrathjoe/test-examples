d59d3132cec0ec9fecec1f87fe06f4ab4194d99e
Joe McGrath <mcgrathjoe@gmail.com>
2014-07-21 19:07:25 +0100

    Added SQL Server DB project, and a couple of tables.

---
+++ TestExamples.Database/Rider.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Rider]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [Surname] NVARCHAR(50) NOT NULL, 
+    [TeamId] INT NOT NULL, 
+    CONSTRAINT [FK_Rider_Team] FOREIGN KEY ([TeamId]) REFERENCES [Team]([Id])
+)
---
+++ TestExamples.Database/Team.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Team]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL
+)
